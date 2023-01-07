2b7e310a0c61fc1f49f2c010864f8dc212ae362b
ElWazy <ElCazero.Fer@gmail.com>
2023-01-06 21:34:17 -0300

    Horas agregadas

--- hours.ts
+++ hours.ts
@@ -2,33 +2,43 @@ import { TimeRange } from "./main.ts";
 
 export const hours: Array<TimeRange> = [
   {
-    init:   new Date(2023, 0, 1, 11, 30, 0),
-    finish: new Date(2023, 0, 1, 14, 30, 0),
+    init: new Date(2023, 0, 3, 11, 30, 0),
+    finish: new Date(2023, 0, 3, 14, 30, 0),
     comentary: 'Reunion de bienvenida y 1ra tarea, crear DER',
   },
   {
-    init:   new Date(2023, 0, 1, 15, 30, 0),
-    finish: new Date(2023, 0, 1, 17, 30, 0),
+    init: new Date(2023, 0, 3, 15, 30, 0),
+    finish: new Date(2023, 0, 3, 17, 30, 0),
     comentary: 'Introduccion a tecnologias, stack AWS y Django',
   },
   {
-    init:   new Date(2023, 0, 2, 12, 30, 0),
-    finish: new Date(2023, 0, 2, 13, 30, 0),
+    init: new Date(2023, 0, 4, 12, 30, 0),
+    finish: new Date(2023, 0, 4, 13, 30, 0),
     comentary: 'Revision DER',
   },
   {
-    init:   new Date(2023, 0, 2, 13, 0, 0),
-    finish: new Date(2023, 0, 2, 15, 25, 0),
+    init: new Date(2023, 0, 4, 13, 0, 0),
+    finish: new Date(2023, 0, 4, 15, 25, 0),
     comentary: 'Creacion mockup no funcional',
   },
   {
-    init:   new Date(2023, 0, 3, 10, 0, 0),
-    finish: new Date(2023, 0, 3, 11, 0, 0),
+    init: new Date(2023, 0, 5, 10, 0, 0),
+    finish: new Date(2023, 0, 5, 11, 0, 0),
     comentary: 'Prototipado de la app',
   },
   {
-    init:   new Date(2023, 0, 3, 15, 0, 0),
-    finish: new Date(2023, 0, 3, 17, 0, 0),
-    comentary: 'Reunion presencial con el equipo',
+    init: new Date(2023, 0, 5, 15, 0, 0),
+    finish: new Date(2023, 0, 5, 17, 0, 0),
+    comentary: 'Reunion presencial con el equipo y Revision mockup',
+  },
+  {
+    init: new Date(2023, 0, 6, 15, 0, 0),
+    finish: new Date(2023, 0, 6, 17, 0, 0),
+    comentary: 'Setup frontend',
+  },
+  {
+    init: new Date(2023, 0, 6, 20, 0, 0),
+    finish: new Date(2023, 0, 6, 21, 0, 0),
+    comentary: 'Enrutamiento y navegacion de la app',
   },
 ]
